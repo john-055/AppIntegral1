a0162341ff2dde1909124adb61dbaaa0179cd729
Alex32451756 <75550479+Alex32451756@users.noreply.github.com>
2022-06-22 23:11:00 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/inicioStripper', function () {
+    return view('components.stripper.inicioStripper');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 
